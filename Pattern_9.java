5a899797973b06e7552066bf1eec64217843db85
Atharva321 <atharvapatil19202@gmail.com>
2021-09-10 22:08:00 +0000

    Horizontal Y pattern

--- Pattern_9.java
+++ Pattern_9.java
@@ -2,12 +2,13 @@
 =======================
 Input:
 5
+-----------------------
 Output:
-1
-2       3
-4       5       6
-7       8       9       10
-11      12      13      14      15
+*
+        *
+                *
+        *               *
+*                               *	
 =======================
 */
 import java.io.BufferedReader;
@@ -16,7 +17,7 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Pattern_11{
+public class Pattern_9{
     static class FastScanner{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer("");
@@ -37,11 +38,14 @@ public class Pattern_11{
     public static void main(String[] args){
         FastScanner s = new FastScanner();
         int n = s.nextInt();
-        int c = 1;
         for(int i=1; i<=n; i++){
             for(int j=1;j<=i;j++){
-                System.out.print(c + "\t");
-                c++;
+                if(i==j || i+j==n+1){
+                System.out.print("*\t");
+                }
+                else {
+                    System.out.print("\t");
+                }
             }
             System.out.println();
         }
