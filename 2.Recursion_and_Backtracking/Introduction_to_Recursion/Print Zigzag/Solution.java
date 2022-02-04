bda7d46119f7252c3c827f422bac8ad6c43d0c3b
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-02-04 23:56:02 +0530

    Create  Solution.java

---
+++ 2.Recursion_and_Backtracking/Introduction_to_Recursion/Print Zigzag/Solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pzz(n);
+    }
+
+    public static void pzz(int n){
+        if(n==0) return;
+        
+        //Pre
+        System.out.print(n + " ");
+        pzz(n-1);
+        
+        //In
+        System.out.print(n + " ");
+        pzz(n-1);
+        
+        //Post
+        System.out.print(n + " ");
+    }
+
+}
