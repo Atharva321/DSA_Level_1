b6365f7ffaa92e05cbb3fa7e97257ac645cb45e9
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-02-05 07:24:35 +0530

    Create Solution.java

---
+++ Stacks And Queues/Normal Stack/Solution.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static class CustomStack {
+    int[] data;
+    int tos;
+
+    public CustomStack(int cap) {
+      data = new int[cap];
+      tos = -1;
+    }
+
+    int size() {
+      // write ur code here
+    }
+
+    void display() {
+      // write ur code here
+    }
+
+    void push(int val) {
+      // write ur code here
+    }
+
+    int pop() {
+      // write ur code here
+    }
+
+    int top() {
+       // write ur code here
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    CustomStack st = new CustomStack(n);
+
+    String str = br.readLine();
+    while(str.equals("quit") == false){
+      if(str.startsWith("push")){
+        int val = Integer.parseInt(str.split(" ")[1]);
+        st.push(val);
+      } else if(str.startsWith("pop")){
+        int val = st.pop();
+        if(val != -1){
+          System.out.println(val);
+        }
+      } else if(str.startsWith("top")){
+        int val = st.top();
+        if(val != -1){
+          System.out.println(val);
+        }
+      } else if(str.startsWith("size")){
+        System.out.println(st.size());
+      } else if(str.startsWith("display")){
+        st.display();
+      }
+      str = br.readLine();
+    }
+  }
+}
