2f1f4f8e6621cdac3d5fd74804deef71914a8f48
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-02-05 07:44:07 +0530

    Update Solution.java

--- Stacks And Queues/Normal Stack/Solution.java
+++ Stacks And Queues/Normal Stack/Solution.java
@@ -14,22 +14,50 @@ public class Main {
 
     int size() {
       // write ur code here
+      return tos + 1;
     }
 
     void display() {
       // write ur code here
+      for(int i=tos; i>=0; i--){
+        System.out.print(data[i] + " ");
+      }
+      System.out.println();
     }
 
     void push(int val) {
       // write ur code here
+      if(tos == data.length-1){ 
+        System.out.println("Stack overflow");
+      }
+      else{
+        tos++;
+        data[tos] = val;
+      }   
     }
 
     int pop() {
       // write ur code here
+      if(tos == -1){ 
+        System.out.println("Stack underflow");
+        return -1;
+      } 
+      else{
+        int val = data[tos];
+        tos--;
+        return val;
+      }
     }
 
     int top() {
        // write ur code here
+       if(tos == -1){ 
+        System.out.println("Stack underflow");
+        return -1;
+      }
+      else{
+       return data[tos];
+      }    
     }
   }
 
