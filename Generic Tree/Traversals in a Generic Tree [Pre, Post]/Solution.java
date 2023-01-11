31441e25454ca9d49d27d279d9710a544bfb16d3
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-01-11 11:48:45 +0530

    Create Solution.java

---
+++ Generic Tree/Traversals in a Generic Tree [Pre, Post]/Solution.java
@@ -0,0 +1,144 @@
+// Time  : O(n)
+// Space : O(1) 
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  public static int size(Node node) {
+    int s = 0;
+
+    for (Node child : node.children) {
+      s += size(child);
+    }
+    s += 1;
+
+    return s;
+  }
+
+  public static int max(Node node) {
+    int m = Integer.MIN_VALUE;
+
+    for (Node child : node.children) {
+      int cm = max(child);
+      m = Math.max(m, cm);
+    }
+    m = Math.max(m, node.data);
+
+    return m;
+  }
+
+  public static int height(Node node) {
+    int h = -1;
+
+    for (Node child : node.children) {
+      int ch = height(child);
+      h = Math.max(h, ch);
+    }
+    h += 1;
+
+    return h;
+  }
+
+/********
+
+Output:
+
+Node Pre 10
+Edge Pre 10--20
+Node Pre 20
+Node Post 20
+Edge Post 10--20
+Edge Pre 10--30
+Node Pre 30
+Edge Pre 30--50
+Node Pre 50
+Node Post 50
+Edge Post 30--50
+Edge Pre 30--60
+Node Pre 60
+Node Post 60
+Edge Post 30--60
+Node Post 30
+Edge Post 10--30
+Edge Pre 10--40
+Node Pre 40
+Node Post 40
+Edge Post 10--40
+Node Post 10
+
+*************/
+  public static void traversals(Node node){
+    // write your code here
+
+    //------------------------PreArea----------------------------------
+    System.out.println("Node Pre " + node.data);
+
+    //=================================================================
+    for(Node child: node.children){
+      //Pre
+      System.out.println("Edge Pre " + node.data + "--" + child.data);
+      traversals(child);
+      //Post
+      System.out.println("Edge Post " + node.data + "--" + child.data);
+    }
+    //=================================================================
+    //------------------------PostArea---------------------------------
+    System.out.println("Node Post " + node.data);
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    Node root = construct(arr);
+    traversals(root);
+  }
+
+}
