1f53e6ff2770c34974b326486dc543ddc8b27a64
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-02-02 22:56:50 +0530

    Update Solution.java

--- Binary Tree/Transform To Left-cloned Tree/Solution.java
+++ Binary Tree/Transform To Left-cloned Tree/Solution.java
@@ -1,1 +1,14 @@
+public static Node createLeftCloneTree(Node node){
+    // write your code here
+    if (node == null) {
+      return null;
+    }
 
+    Node leftcloned = createLeftCloneTree(node.left);
+    Node rightcloned = createLeftCloneTree(node.right);
+
+    node.left = new Node(node.data, leftcloned, null);
+    node.right = rightcloned;
+
+    return node;
+  }
