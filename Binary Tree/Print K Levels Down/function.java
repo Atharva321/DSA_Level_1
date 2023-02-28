8c3aa34daca1fd1ebbecb39b4d1d4b1fa565bf29
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-02-28 23:05:53 +0530

    Create function.java

---
+++ Binary Tree/Print K Levels Down/function.java
@@ -0,0 +1,14 @@
+ public static void printKLevelsDown(Node node, int k) {
+    if (node == null || k < 0) {
+      return;
+    }
+
+    if (k == 0) {
+      System.out.println(node.data);
+      return;
+    }
+   
+    printKLevelsDown(node.left, k - 1);
+    //recurse
+    printKLevelsDown(node.right, k - 1);
+  }
